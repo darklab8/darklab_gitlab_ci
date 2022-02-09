abc81b256079805403ef848dea056ca6cbbabe49
dd84ai <dd84ai@gmail.com>
2022-02-09 12:58:12 +0700

    feat: DIND with access to registry

--- k8s/gitlab_runner/install_runner.py
+++ k8s/gitlab_runner/install_runner.py
@@ -28,4 +28,4 @@ registrational_token_base64 = str(base64.b64encode((args.gitlab_runner_registry_
 print(registrational_token_base64)
 # kubectl config set-context --current --namespace=
 shell(f'helm upgrade --install --create-namespace --namespace gitlab-runner gitlab-secret . --set super_secret_token="{registrational_token_base64}"')
-shell(f"helm upgrade --install --create-namespace --namespace gitlab-runner -f values.yaml gitlab-runner gitlab/gitlab-runner")
+shell(f"helm upgrade --install --create-namespace --namespace gitlab-runner -f gitlab.yaml gitlab-runner gitlab/gitlab-runner")
